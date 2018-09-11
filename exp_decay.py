ad88e4b07290328d13d1e44c66df78e6e23df29b
bdnguye <bdnguye@student.matnat.uio.no>
2018-09-11 11:17:30 +0200

    Still can not solve 1b missing dudt in solve

--- exp_decay.py
+++ exp_decay.py
@@ -1,24 +1,20 @@
 import numpy as np
 import scipy.integrate as sp
+
 class ExponentialDecay(object):
-    """Lalallalalaalaaaa, blablalba, lalaaa"""
+    """Solves the exponential decay ODE"""
     def __init__(self, a):
         self.a = a
     def __call__(self, t, u):
         dudt = -self.a*u
+        print('test')
         return(dudt)
     def solve(self, u0, T, dt):
-        t = range(0,T+1,dt)
-        ut = sp.solve_ivp(dudt,t,t_eval=array_like)
+        #t = np.linspace(0,T+1,dt)
+        t = (0,T)
+        #u = u0
+        #dudt = ExponentialDecay.call(t,u)
+        #dudt=call(t,u0)
+        print('heihei')
+        ut = sp.solve_ivp(dudt,t,u0,t_eval=None)
         return(t,ut)
-
-a = 0.4
-u0 = [3.2]
-T = 5
-dt = 101
-
-decay_model = ExponentialDecay(a)
-t, u = decay_model.solve(u0, T, dt)
-
-plt.plot(t, u)
-plt.show()
