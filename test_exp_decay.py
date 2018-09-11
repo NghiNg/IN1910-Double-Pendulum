ab65f894060c39a6362de0234c4934cf94a38f41
runestol <runestol@student.matnat.uio.no>
2018-09-11 11:46:07 +0200

    imported plt

--- test_exp_decay.py
+++ test_exp_decay.py
@@ -1,10 +1,11 @@
 from exp_decay import ExponentialDecay
 from numpy import linspace
+import matplotlib.pyplot as plt
 
 a = 0.4
 u0 = (3.2,)
 T = 5
-dt = 2
+dt = 10
 
 decay_model = ExponentialDecay(a)
 t, u = decay_model.solve(u0, T, dt)
