8e9dab79d4f302e1bcd543042152fa610cbce39b
bdnguye <bdnguye@student.matnat.uio.no>
2018-09-11 11:59:16 +0200

    Moved things from test file it still doesnt run but the class is correct.

--- exp_decay.py
+++ exp_decay.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.integrate as sp
+import matplotlib.pyplot as plt
 
 class ExponentialDecay(object):
     """Solves the exponential decay ODE"""
@@ -12,3 +13,14 @@ class ExponentialDecay(object):
         t = np.linspace(0,T+1,dt)
         ut = sp.solve_ivp(self, (0,T+1), u0, t_eval=t)
         return(t,ut)
+
+a = 0.4
+u0 = (3.2,)
+T = 5
+dt = 10
+
+decay_model = ExponentialDecay(a)
+t, u = decay_model.solve(u0, T, dt)
+
+plt.plot(t, u)
+plt.show()
