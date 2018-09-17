56afe3e17eeceddf9f90c19973fba3506292713c
runestol <runestol@student.matnat.uio.no>
2018-09-17 12:08:01 +0200

    added energy and motion plots

--- pendulum.py
+++ pendulum.py
@@ -3,7 +3,7 @@
 
 import scipy.integrate as sp
 import numpy as np
-import math
+import matplotlib.pyplot as plt
 
 g = 9.81        # gravitational acceleration [m/s^2]
 
@@ -13,14 +13,20 @@ class Pendulum:
         self.M = M      # mass of pendulum [kg]
 
     def __call__(self, t, y):
-        '''y is a tuple of (theta, omega)'''
+        '''
+        t: an array of time steps from (0,T)
+        y: a tuple of (theta, omega)
+        '''
         dthetadt = y[1]
         domegadt = -(g/self.L)*np.sin(y[0])
         return(dthetadt, domegadt)
 
     def solve(self, y0, T, dt, angles='rad'):
-        '''y0 is tuple of (theta0, omega0)
-        angles can be 'rad' for radians or 'deg' for degrees.'''
+        '''
+        y0:     A tuple of (theta0, omega0)
+        T:      Timespan
+        angles: Optional keyword argument, by default 'rad' for radians or set to 'deg' for degrees
+        '''
         time = np.linspace(0,T+1,dt)
         if angles == 'deg':
             math.radians(y0[0])
@@ -28,49 +34,67 @@ class Pendulum:
             pass
         else:
             raise NameError('Keyword angles must be rad or deg as string.')
-        self.solution = sp.solve_ivp(self, (0,T+1), y0, t_eval=time)
-        '''self.t = solution.t
-        self.theta, self.omega = solution.y'''
-        self._t = self.solution.t
-        self._theta = self.solution.y[0]
-        self._omega = self.solution.y[1]
+        solution = sp.solve_ivp(self, (0,T+1), y0, t_eval=time)
+        self._t = solution.t
+        self._theta = solution.y[0]
+        #self._omega = solution.y[0]
+
     @property
     def t(self):
         return self._t
     @t.setter
     def t(self,value):
         self._t = value
+
     @property
     def theta(self):
         return self._theta
     @theta.setter
     def theta(self,value):
         self._theta = value
+
     @property
     def omega(self):
         return self._omega
     @omega.setter
     def omega(self,value):
         self._omega = value
+
     @property
     def x(self):
         self._x = self.L*np.sin(self._theta)
         return self._x
+
     @property
     def y(self):
         self._y = (-1)*self.L*np.cos(self._theta)
         return self._y
+
     @property
     def v(self):
         vx = np.gradient(self.x, self.t)
         vy = np.gradient(self.y, self.t)
         self._v = np.asarray([vx, vy])
         return self._v
+
     @property
     def potential(self):
-        self._P = self.M*g*(self._y + self.L)
+        self._P = self.M*g*(self.y + self.L)
         return self._P
+
     @property
     def kinetic(self):
-        self._K = 0.5*self.M*(self._v[0]**2 + self._v[1]**2)
+        self._K = 0.5*self.M*(self.v[0]**2 + self.v[1]**2)
         return self._K
+
+a = Pendulum()
+a.solve((np.pi/4, 0.1), 10, 1001)
+
+plt.figure('motion')
+plt.plot(a.t, a.theta, 'k')
+plt.figure('energy')
+plt.plot(a.t, a.kinetic, color='darkcyan')
+plt.plot(a.t, a.potential, color='firebrick')
+plt.plot(a.t, a.kinetic + a.potential, color='orange')
+
+plt.show()
