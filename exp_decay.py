e6026fa3e8182f8196e88bf6a0077195d8accd6c
runestol <runestol@student.matnat.uio.no>
2018-09-17 12:27:14 +0200

    added some comments

--- exp_decay.py
+++ exp_decay.py
@@ -3,22 +3,26 @@ import scipy.integrate as sp
 import matplotlib.pyplot as plt
 
 class ExponentialDecay(object):
-    """Solves the exponential decay ODE"""
+    '''Solves the ODE for exponential decay'''
     def __init__(self, a):
         self.a = a
+
     def __call__(self, t, u):
         dudt = -self.a*u
         return(dudt)
+
     def solve(self, u0, T, dt):
         t = np.linspace(0,T+1,dt)
         ut = sp.solve_ivp(self, (0,T+1), u0, t_eval=t)
         return(t,ut)
+
 
 a = 0.4
 u0 = (3.2,)
 T = 5
 dt = 10
 
+# plotting solution
 decay_model = ExponentialDecay(a)
 t, u = decay_model.solve(u0, T, dt)
 plt.plot(t, u.y[0])
