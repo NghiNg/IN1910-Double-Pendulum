77e622e777457bdd57cdbf2a4edb0d1bbc50a090
bdnguye <bdnguye@student.matnat.uio.no>
2018-09-18 11:55:01 +0200

    Added zero test.

--- test_double_pendulum.py
+++ test_double_pendulum.py
@@ -15,3 +15,13 @@ def test_radius2():
     r2 = a.x2**2 + a.y2**2
     assert np.isclose(1**2, np.all(r2))
 test_radius2()
+
+def test_arrays_zero():
+    '''
+    test-function for checking if initial conditions y0=(0,0) yields zero-filled arrays
+    '''
+    a = DoublePendulum()
+    a.solve((0.0, 0, 0.0, 0), 10, 1001)
+    assert np.all(a.theta == 0) and np.all(a.omega == 0), 'initial conditions \
+           y0=(0,0) does not yield arrays with zeros'
+test_arrays_zero()
