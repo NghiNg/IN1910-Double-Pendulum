e3b2d16491d65a952ebed256c35bac1d53509368
runestol <runestol@student.matnat.uio.no>
2018-09-18 11:14:49 +0200

    added test function docstrings

--- test_pendulum.py
+++ test_pendulum.py
@@ -6,6 +6,9 @@ y = (np.pi/4, 1)
 t = np.linspace(0,100,101)
 
 def test_pendulum1():
+    '''
+    testing if equations of motion is implemented properly
+    '''
     L = 2.2
     ans = -3.15305342
     a = Pendulum(L)
@@ -14,6 +17,9 @@ def test_pendulum1():
 test_pendulum1()
 
 def test_pendulum2():
+    '''
+    checking if pendulum with initial conditions at rest stays at rest
+    '''
     theta = 0
     omega = 0
     ans = 0
@@ -23,6 +29,9 @@ def test_pendulum2():
 test_pendulum2()
 
 def test_radius():
+    '''
+    testing for a constant rope length
+    '''
     a = Pendulum()
     a.solve((0,0.1),10,101)
     r2 = a.x**2 + a.y**2
@@ -30,16 +39,29 @@ def test_radius():
 test_radius()
 
 def test_solve_called_t():
+    '''
+    deliberately raising exception for when solve-method is not called
+    '''
     a = Pendulum()
     a.t
 test_solve_called_t()
 
 def test_solve_called_theta():
+    '''
+    deliberately raising exception for when solve-method is not called
+    '''
     a = Pendulum()
     a.theta
 test_solve_called_theta()
 
 def test_solve_called_omega():
+    '''
+    deliberately raising exception for when solve-method is not called
+    '''
     a = Pendulum()
     a.omega
 test_solve_called_omega()
+
+def test_arrays_zero():
+    
+test_arrays_zero()
